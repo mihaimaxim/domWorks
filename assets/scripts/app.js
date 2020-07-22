47903779fb00bb4362fc53beea9e68010269a1b6
Mihai Maxim <mihaimaxim90@icloud.com>
2020-07-22 08:44:34 +0300

    test

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,8 +5,24 @@ const cancelMovieModalButton = movieModal.querySelector(".btn--passive");
 const confirmMovieModalButton = cancelMovieModalButton.nextElementSibling;
 const userInputs = movieModal.querySelectorAll("input");
 const entryText = document.getElementById("entry-text");
+const movieList = document.getElementById("movie-list");
 
 let movies = [];
+
+populateMovieList = (title, imgURL, rating) => {
+   const movieElement = document.createElement("li");
+   movieElement.className = "movie-element";
+   movieElement.innerHTML = `
+   <div class="movie-element__image">
+      <img src="${imgURL}" alt="${title}" />
+   </div>
+   <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+   </div>
+   `;
+   movieList.appendChild(movieElement);
+};
 
 updateUI = () => {
    if (movies.length === 0) {
@@ -33,12 +49,12 @@ clearInputs = () => {
 
 confirmMovieModal = () => {
    const titleInput = userInputs[0].value;
-   const urlInput = userInputs[1].value;
+   const imgUrlInput = userInputs[1].value;
    const ratingInput = userInputs[2].value;
 
    if (
       titleInput.trim() === "" ||
-      urlInput.trim() === "" ||
+      imgUrlInput.trim() === "" ||
       ratingInput.trim() === "" ||
       ratingInput.trim() <= 0 ||
       ratingInput.trim() > 5
@@ -49,16 +65,17 @@ confirmMovieModal = () => {
 
    let movie = {
       title: titleInput,
-      url: urlInput,
+      url: imgUrlInput,
       rating: ratingInput,
    };
 
    movies.push(movie);
-   console.log(movies[0]);
+   console.log(movies);
    displayModal();
    // movies = [];
    clearInputs();
    updateUI();
+   populateMovieList(titleInput, imgUrlInput, ratingInput);
 };
 
 cancelMovieModal = () => {
